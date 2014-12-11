73c96c19737bdd2187ac9ae0d1466291e0d848b9
Sayzana Saryglar <whitechaos91@mail.ru>
2014-12-11 17:09:47 +0300

    change scss, add opacity-range.js | Sayzana Saryglar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -207,7 +207,89 @@ var moveWatermark = (function() {
 	// возвращаем объект с публичными методами
 	return {};
 }());
-;'use strict';
+;var opacityRange = (function () {
+  // Обьявляем пустые переменные, для последующей с ними работы
+  var
+      app,
+      self,
+      flag,
+      toggle,
+      scale,
+      bar,
+      cursorPosX,
+      lastPosX,
+      watermarkOpacity,
+      scalePosX,
+      scaleWidth,
+      scalePosStartX,
+      scalePosEndX,
+      toggleWidth;
+
+  // объект с приватными методами
+  app = {
+    // метод инициалицации модуля
+    init: function () {
+      flag = false,
+      toggle = $('.toggle'),
+      scale = $('.scale'),
+      bar = $('.bar'),
+      cursorPosX,
+      lastPosX,
+      watermarkOpacity,
+      scalePosStartX = parseInt(scale.offset().left),
+      toggleWidth =  parseInt(toggle.css('width')),
+      scalePosEndX = scalePosStartX + parseInt(scale.css('width')) - toggleWidth,
+      toggleWidth =  parseInt(toggle.css('width'));
+
+      self = this;
+
+      // вызываем метод который понавешает события модуля
+      self.events();
+      // Также тут вызываем методы которые нужны для инициализации
+    },
+
+    // метод содержащий все события модуля
+    events: function () {
+      toggle.on('mousedown', function () {
+        flag = true;
+      });
+
+      $('.range-controls').on('mousemove', function (e) {
+        cursorPosX = e.pageX;
+
+        if (flag) {
+
+          if ((cursorPosX > scalePosStartX) && (cursorPosX < scalePosEndX)) {
+
+            toggle.offset({left: cursorPosX});
+            lastPosX = parseInt(toggle.css('left'));
+            bar.css('width', lastPosX + 'px');
+            $('.wm').css('opacity', lastPosX / 300);
+
+          }
+        }
+      });
+
+      $(document).on('mouseup', function () {
+        flag = false;
+      });
+
+      scale.on('click', function () {
+        if ((cursorPosX > scalePosStartX) && (cursorPosX < scalePosEndX)) {
+
+          toggle.offset({left: cursorPosX});
+          lastPosX = parseInt($(toggle).css('left'));
+          bar.css('width', lastPosX + 'px');
+          $('.wm').css('opacity', lastPosX / 300);
+
+        }
+      });
+    }
+  };
+
+// инициализируем модуль
+app.init();
+}());;'use strict';
 
 var module = (function() {
     var
