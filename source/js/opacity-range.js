aab71e5843518c91e9d3a11c3d3c85eb3b9d43ab
Sayzana Saryglar <whitechaos91@mail.ru>
2014-12-17 18:24:39 +0300

    refactoring opacity-range.js | Sayzana

--- source/js/opacity-range.js
+++ source/js/opacity-range.js
@@ -1,33 +1,36 @@
 var app = angular.module('drag', []);
 
-app.controller('DragCtrl', ['$scope', function($scope) {
-  
+app.controller('DragCtrl', ['$scope',
+  function ($scope) {
+
 }]);
 
-app.directive('draggable', ['$document', function($document) {
-  return {
-          restrict: 'C',
-          link: function(scope, element, attr) {
-            var startX = 0, x = 0;
+app.directive('draggable', ['$document',
+  function ($document) {
+    return {
+      restrict: 'C',
+      link: function (scope, element, attr) {
+        var startX = 0,
+          x = 0;
 
-            element.bind('mousedown', function(event) {
-              event.preventDefault();
-              startX = event.screenX - x;
-              $document.bind('mousemove', mousemove);
-              $document.bind('mouseup', mouseup);
-            });
+        element.bind('mousedown', function (event) {
+          event.preventDefault();
+          startX = event.screenX - x;
+          $document.bind('mousemove', mousemove);
+          $document.bind('mouseup', mouseup);
+        });
 
-            function mousemove(event) {
-              x = event.screenX - startX;
-              element.css({
-                left: x + 'px'
-              });
-            }
+        function mousemove(event) {
+          x = event.screenX - startX;
+          element.css({
+            left: x + 'px'
+          });
+        }
 
-            function mouseup() {
-              $document.unbind('mousemove', mousemove);
-              $document.unbind('mouseup', mouseup);
-            }
-          }
-        };
+        function mouseup() {
+          $document.unbind('mousemove', mousemove);
+          $document.unbind('mouseup', mouseup);
+        }
+      }
+    };
 }]);
