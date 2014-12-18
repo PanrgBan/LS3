e020edb4b0a86a2201bd91e454d12e068cc2ad21
Sayzana Saryglar <whitechaos91@mail.ru>
2014-12-18 18:26:13 +0300

    change opacity-range.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -361,39 +361,68 @@ var moveWatermark = (function() {
 	// возвращаем объект с публичными методами
 	return {};
 }());
-;var app = angular.module('drag', []);
-
-app.controller('DragCtrl', ['$scope', function($scope) {
+;var opacityRange = (function() {
+    var
+        app,
+        self,
+        startX,
+        x,
+        toggle,
+        scale,
+        bar,
+        lastPosX,
+        cursorX,
+        $document;
   
-}]);
+   app = {
+    // метод инициалицации модуля
+    init: function() {
+      startX = 0,
+      x = 0,
+      toggle = $('.toggle'),
+      scale = $('.scale'),
+      bar = $('.bar'),
+      $document = $(document),
+      lastPosX = 0,
+      cursorX,
+      self = this;
 
-app.directive('draggable', ['$document', function($document) {
-  return {
-          restrict: 'C',
-          link: function(scope, element, attr) {
-            var startX = 0, x = 0;
+      self.events();
+    },
 
-            element.bind('mousedown', function(event) {
-              event.preventDefault();
-              startX = event.screenX - x;
-              $document.bind('mousemove', mousemove);
-              $document.bind('mouseup', mouseup);
-            });
+    // метод содержащий все события модуля
+    events: function() {
+      
+      toggle.bind('mousedown', function (event) {
+        event.preventDefault();
+        console.log(event.screenX);
+        startX = event.screenX - x;
+        $document.bind('mousemove', mousemove);
+        $document.bind('mouseup', mouseup);
+      });
 
-            function mousemove(event) {
-              x = event.screenX - startX;
-              element.css({
-                left: x + 'px'
-              });
-            }
+      function mousemove(event) {
+        x = event.screenX - startX;
+        if ((x > -5) && (x < toggle.parent()[0].offsetWidth - 15)) {
+          toggle.css({
+          left: x + 'px'
+          });
+          
+        lastPosX = parseInt(toggle.css('left'));
+        bar.css('width', lastPosX + 'px');
+        $('.wm').css('opacity', lastPosX / toggle.parent()[0].offsetWidth);
+        }
+      };
 
-            function mouseup() {
-              $document.unbind('mousemove', mousemove);
-              $document.unbind('mouseup', mouseup);
-            }
-          }
-        };
-}]);;'use strict';
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      };
+    },
+     
+  };
+  app.init();
+}());;'use strict';
 
 var module = (function() {
     var
--- source/js/opacity-range.js
+++ source/js/opacity-range.js
@@ -1,36 +1,62 @@
-var app = angular.module('drag', []);
+var opacityRange = (function() {
+    var
+        app,
+        self,
+        startX,
+        x,
+        toggle,
+        scale,
+        bar,
+        lastPosX,
+        cursorX,
+        $document;
+  
+   app = {
+    // метод инициалицации модуля
+    init: function() {
+      startX = 0,
+      x = 0,
+      toggle = $('.toggle'),
+      scale = $('.scale'),
+      bar = $('.bar'),
+      $document = $(document),
+      lastPosX = 0,
+      cursorX,
+      self = this;
 
-app.controller('DragCtrl', ['$scope',
-  function ($scope) {
+      self.events();
+    },
 
-}]);
+    // метод содержащий все события модуля
+    events: function() {
+      
+      toggle.bind('mousedown', function (event) {
+        event.preventDefault();
+        console.log(event.screenX);
+        startX = event.screenX - x;
+        $document.bind('mousemove', mousemove);
+        $document.bind('mouseup', mouseup);
+      });
 
-app.directive('draggable', ['$document',
-  function ($document) {
-    return {
-      restrict: 'C',
-      link: function (scope, element, attr) {
-        var startX = 0,
-          x = 0;
-
-        element.bind('mousedown', function (event) {
-          event.preventDefault();
-          startX = event.screenX - x;
-          $document.bind('mousemove', mousemove);
-          $document.bind('mouseup', mouseup);
-        });
-
-        function mousemove(event) {
-          x = event.screenX - startX;
-          element.css({
-            left: x + 'px'
+      function mousemove(event) {
+        x = event.screenX - startX;
+        if ((x > -5) && (x < toggle.parent()[0].offsetWidth - 15)) {
+          toggle.css({
+          left: x + 'px'
           });
+          
+        lastPosX = parseInt(toggle.css('left'));
+        bar.css('width', lastPosX + 'px');
+        $('.wm').css('opacity', lastPosX / toggle.parent()[0].offsetWidth);
         }
+      };
 
-        function mouseup() {
-          $document.unbind('mousemove', mousemove);
-          $document.unbind('mouseup', mouseup);
-        }
-      }
-    };
-}]);
+      function mouseup() {
+        $document.unbind('mousemove', mousemove);
+        $document.unbind('mouseup', mouseup);
+      };
+    },
+     
+  };
+  app.init();
+}());
